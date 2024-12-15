05f2305d40009123a488214e2140fcb283c5b288
Kitti U <kittiu@gmail.com>
2024-12-15 09:46:06 +0700

    /

--- frappe_training/frappe_training/doctype/ticket/ticket.js
+++ frappe_training/frappe_training/doctype/ticket/ticket.js
@@ -3,11 +3,20 @@
 
 frappe.ui.form.on("Ticket", {
 	refresh(frm) {
+        // On draft and not yet auto assigned, show Auto Assignment button
         if (frm.doc.docstatus == 0 && !frm.doc.auto_assign) {
             frm.add_custom_button(__("Auto Assignment"), () => {
 				frm.set_value("auto_assign", 1);
                 frm.save();
 			});
+        }
+        // On resolved and not yet used, show Create Ticket Summary button
+        if (frm.doc.status == 'Resolved' && frm.doc.customer && !frm.doc.ref_ticket_summary) {
+            frm.add_custom_button(__("Create Ticket Summary"), () => {
+                frappe.new_doc("Ticket Summary", {
+                    customer: frm.doc.customer
+                })
+            });
         }
 	},
 });
--- frappe_training/public/js/customer.js
+++ frappe_training/public/js/customer.js
@@ -5,10 +5,15 @@ frappe.ui.form.on("Customer", {
 
 	refresh: function (frm) {
         frm.add_custom_button(__("Create Ticket Summary"), () => {
-            frappe.route_options = {
-                "customer": frm.doc.name
-            };
-            frappe.set_route("Form", "Ticket Summary", "new");
+            frappe.new_doc("Ticket Summary", {
+                customer: frm.doc.name
+            })
+            // ---- Following code is another way to do the same ----
+            // frappe.route_options = {
+            //     "customer": frm.doc.name
+            // };
+            // frappe.set_route("Form", "Ticket Summary", "new");
+            // ------------------------------------------------------
         }, "Actions");
 	},
 
