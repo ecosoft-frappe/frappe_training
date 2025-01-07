23b287d269a5a62e4aeba16f34ae3e7dfd1b10a5
Kitti U <kittiu@gmail.com>
2025-01-07 20:40:36 +0700

    /

--- frappe_training/frappe_training/doctype/ticket_summary/ticket_summary.py
+++ frappe_training/frappe_training/doctype/ticket_summary/ticket_summary.py
@@ -8,16 +8,13 @@ from frappe.model.document import Document
 
 class TicketSummary(Document):
 
-
 	def on_submit(self):
 		for t in self.tickets:
 			if frappe.db.get_value("Ticket", t.ticket, "ref_ticket_summary"):
 				frappe.throw("Ticket {0} already linked to another Ticket Summary".format(t.ticket))
 			frappe.db.set_value("Ticket", t.ticket, "ref_ticket_summary", self.name)
 
-
 	def before_cancel(self):
-		# self.flags.ignore_links = True
 		for t in self.tickets:
 			frappe.db.set_value("Ticket", t.ticket, "ref_ticket_summary", "")
 
